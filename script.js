0709e0a18f2f9d12cf439edad43e056e34c78f73
Sushant Sharma <79782587+sushantsharma0221@users.noreply.github.com>
2022-02-01 11:04:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let input=document.getElementById("input");
 input.addEventListener("input",function(e){
     document.getElementById('output').style.visibility="visible";
     let kg= e.target.value;
-    document.getElementById("gram").innerHTML += " "+kg*1000;
-    document.getElementById("pound").innerHTML += " "+kg*2.204;
-    document.getElementById("ounce").innerHTML += " "+kg*35.27;
+    document.getElementById("gram").innerHTML = kg*1000+" gram";
+    document.getElementById("pound").innerHTML = kg*2.204+" pounds";
+    document.getElementById("ounce").innerHTML = kg*35.27+" ounces";
 }
 );
